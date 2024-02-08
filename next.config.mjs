cb8307fa5459b389183e1f58df0a01ee58823872
Sung YeIn <syi9397@naver.com>
2024-02-08 23:58:21 +0900

    Remove tailwind and font, Add stylex

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+import path from 'path';
+import * as url from 'url';
+import stylexPlugin from '@stylexjs/nextjs-plugin';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default stylexPlugin({
+  aliases: {
+    '@/*': [path.join(__dirname, '*')],
+  },
+  rootDir: __dirname,
+})(nextConfig);
